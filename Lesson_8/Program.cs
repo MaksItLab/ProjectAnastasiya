bf587bb70e7ccc9192eb39c577a7bd89956b2d82
MaksItLab <maksim.ardintsev@mail.ru>
2025-05-04 12:37:15 +0300

    Классы и структуры

--- Lesson_8/Program.cs
+++ Lesson_8/Program.cs
@@ -75,30 +75,119 @@ namespace Lesson_8
 
             //num = list.Find(x => x < 10);
             #endregion
-            // 1 - один
-            // 2 - два
-            // 3 - три
-            Dictionary<int, string> dict = new Dictionary<int, string>();
-            dict.Add(1, "Один");
-            //dict.Add(1, "Первый"); - error
-            dict[2] = "Два";
-            dict[2] = "Второй";
+            #region Dictionary
+            //// 1 - один
+            //// 2 - два
+            //// 3 - три
+            //Dictionary<int, string> dict = new Dictionary<int, string>();
+            //dict.Add(1, "Один");
+            ////dict.Add(1, "Первый"); - error
+            //dict[2] = "Два";
+            //dict[2] = "Второй";
 
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-            }
+            //foreach (var item in dict)
+            //{
+            //    Console.WriteLine($"{item.Key} - {item.Value}");
+            //}
 
-            foreach (var item in dict.Keys)
-            {
-                
-            }
+            //foreach (var item in dict.Keys)
+            //{
 
-            foreach (var item in dict.Values)
-            {
+            //}
 
-            }
+            //foreach (var item in dict.Values)
+            //{
 
+            //}
+            #endregion
+            #region Stack
+
+            //Stack<int> stack = new Stack<int>();
+            //stack.Push(element); - добавление
+            //stack.Peek(); - возвращает элемент в начале стэка
+            //stack.Pop(); - Peek + удаление этого элемента из стэка
+
+            //stack.Push(1);
+            //stack.Push(2);
+            //stack.Push(3);
+            //stack.Push(4);
+            //stack.Push(5);
+
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine();
+
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine();
+
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine();
+
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine();
+
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.Pop());
+            //Console.WriteLine(stack[0]); - error
+            //int length = stack.Count;
+            //Stack<int> stackTemp = new Stack<int>();
+            //for (int i = 0; i < length; i++)
+            //{
+            //    stackTemp.Push(stack.Peek());
+            //    Console.WriteLine(stack.Pop());
+            //}
+
+            //length = stackTemp.Count;
+            //for (int i = 0; i < length; i++)
+            //{
+            //    stack.Push(stackTemp.Pop());
+            //}
+
+            #endregion
+
+            Recursive();
+
+            Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(element) - добавление элемента в конец очереди
+            //queue.Peek() - возвращает первый элемент из очереди
+            //queue.Dequeue() - Peek + удаление элемента из начала очереди
+            
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            var temp = queue.Take(queue.Count).ToList();
+            
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine();
+
+        }
+
+        public static void Recursive()
+        {
+            Recursive();
         }
 
         public static void PrintLenght(List<int> list)
---
+++ Lesson_9/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9
+{
+    internal class Program
+    {
+        public static int a = 18;
+        // public
+        // protected
+        // internal
+        // private
+        static void Main(string[] args)
+        {
+            #region Class Structure
+            //Person person1 = new Person();
+            //person1.age = 18;
+            //person1.login = "login123";
+            //person1.password = "secret123";
+
+            //Person person2 = person1;
+
+            //person2.login = "null";
+
+            //Console.WriteLine($"Person1. Login: {person1.login}, Password: {person1.password}");
+            //Console.WriteLine($"Person2. Login: {person2.login}, Password: {person2.password}");
+
+            //PersonStruct personStruct1 = new PersonStruct();
+            //personStruct1.age = 18;
+            //personStruct1.login = "login123";
+            //personStruct1.password = "secret123";
+
+            //PersonStruct personStruct2 = personStruct1;
+
+            //personStruct2.login = "null";
+
+            //Console.WriteLine($"PersonStruct1. Login: {personStruct1.login}, Password: {personStruct1.password}");
+            //Console.WriteLine($"PersonStruct2. Login: {personStruct2.login}, Password: {personStruct2.password}");
+            #endregion
+
+            // Принципы ООП:
+            // 1. Инкапсуляция - private
+            // 2. Наследование
+            // 3. Полиморфизм
+            // 4. Абстракция
+            Print();
+            Person person1 = new Person();
+
+            Console.WriteLine("Логин: " + person1.GetLogin());
+
+            person1.SetLogin("newlogin");
+
+            Console.WriteLine("Логин: " + person1.GetLogin());
+        }
+        public static void Print()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Класс человека
+    /// </summary>
+    public class Person
+    {
+        private int age = 18;
+        private string login = "login123";
+        private string password;
+
+        public string GetLogin() => login;
+
+        public void SetLogin(string log) => login = log;
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine(age);
+        }
+        
+    }
+
+    /// <summary>
+    /// Структура человека
+    /// </summary>
+    public struct PersonStruct
+    {
+        public int age;
+        public string login;
+        public string password;
+    }
+
+}
