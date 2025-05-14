327a4e740e6efda5d7402ec2fd7ec4d557385d81
MaksItLab <maksim.ardintsev@mail.ru>
2025-05-14 20:31:46 +0300

    Разобрали свойства и автосвойства

---
+++ Lesson_11/Entities/TaskBase.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11.Entities
+{
+    internal class TaskBase
+    {
+        //public string Id; // поле (переменная) 
+        public string Id { get; set; } // автосвойство ~ свойство
+
+        private string password = "secret123";
+
+        //public string GetPass() => password;
+
+        public string Password
+        {
+            //возвращает значение
+            get
+            {
+                return password;
+            }
+            //устанавливает значение
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Ошибка! Пароль пустой");
+                    return;
+                }
+                password = value;
+
+            }
+        }
+
+        /// <summary>
+        /// Статус задачи
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; set; }
+
+        public int? Order { get; set; }
+    }
+}
---
+++ Lesson_11/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Lesson_11.Entities;
+
+namespace Lesson_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskBase task = new TaskBase();
+            Console.WriteLine(task.Password); // get
+            task.Password = "newsecret123"; // set
+            Console.WriteLine(task.Password); // get
+
+            task.Id = "nvt9348yt9874qbtn984t3787HIUH98292"; // set
+            Console.WriteLine(task.Id); // get
+
+            // задаем значения свойствам (НЕправильно)
+            //task.Status = "В процессе";
+            //task.Name = "Добавление сущности User";
+            //task.Description = "description";
+
+            //задаем значения свойствам (Правильно)
+            TaskBase task2 = new TaskBase()
+            {
+                Id = "dkjfjksdfhb32gr7867472bgdg",
+                Name = "Удалить сущность User",
+                Description = "description",
+                Status = "Выполнено"
+            };
+            task2.Password = "";
+            Console.WriteLine(task2.Password);
+            task2.Status = "Не выполнено";
+
+            // nullable (?)
+            // ?? - проверка на null
+            // ! - гарантия, что объект не равен null
+            
+            int? age = 16;
+            task2.Order = 10;
+            Console.WriteLine(task2.Order ?? 0);
+            //if (task2.Order != null)
+            //{
+            //    Console.WriteLine(task2.Order);
+            //}
+            //else Console.WriteLine("Пустое значение");
+            
+
+
+
+        }
+    }
+}
