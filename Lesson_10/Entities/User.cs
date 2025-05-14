20fe112c7188a8fd14276dfe76d5f62307cb8b2a
MaksItLab <maksim.ardintsev@mail.ru>
2025-05-14 19:16:06 +0300

    Занятие 10

---
+++ Lesson_10/Entities/User.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10.Entities
+{
+    public class User
+    {
+        private int _age;
+        private string _name;
+        private string _email;
+        private string _password;
+        private string _login;
+
+        /// <summary>
+        /// конструктор
+        /// </summary>
+        /// <param name="age">Возраст</param>
+        /// <param name="name">Имя</param>
+        /// <param name="email">Почта</param>
+        /// <param name="password">Пароль</param>
+        /// <param name="login">Логин</param>
+        public User(int age, string name, string email, string password, string login)
+        {
+            _age = age;
+            _name = name;
+            _email = email;
+            _password = password;
+            _login = login;
+        }
+
+        public User()
+        {
+
+        }
+
+        /// <summary>
+        /// Деструктор
+        /// </summary>
+        ~User()
+        {
+            Console.WriteLine("Объект удален");
+            Console.WriteLine(this._name);
+        }
+
+
+
+
+        /// <summary>
+        /// Метод, возвращающий значения возраста
+        /// </summary>
+        /// <returns>Возраст пользователя</returns>
+        public int GetAge() => _age;
+
+        public void Print(int age)
+        {
+
+        }
+        public void Print(string name)
+        {
+
+        }
+        public void Print(int age, string name)
+        {
+
+        }
+        public void Print(string name, int age)
+        {
+
+        }
+    }
+}
---
+++ Lesson_10/Program.cs
@@ -0,0 +1,37 @@
+﻿using Lesson_10.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15;
+            string name = "Bob";
+            string email = "mail@mail.com";
+            string password = "12345";
+            string login = "login";
+
+            User user = new User(15, name, email, password, "login");
+            User user1 = new User();
+           
+            Order order = new Order();
+
+            //user = null;
+            user1 = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //user.Print("Объект удален");
+
+            Console.WriteLine("Конец программы");
+            
+        }
+    }
+}
