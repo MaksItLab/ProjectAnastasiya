d79743483e1dc0cee5251c68990a4f3974ff8c46
MaksItLab <maksim.ardintsev@mail.ru>
2025-07-13 08:04:35 +0300

    решали дз

--- Lesson_18/Program.cs
+++ Lesson_18/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Lesson_18
@@ -66,7 +67,41 @@ namespace Lesson_18
                 }
             }
 
+            Console.WriteLine("Введите заменяемое слово:");
+            string oldWord = Console.ReadLine();
+            Console.WriteLine("Введите новое слово:");
+            string newWord = Console.ReadLine();
 
+            ReplaceWordInFile("words.txt", oldWord, newWord);
+
+            //валидация - проверка на корректность данных
         }
+
+        public static void ReplaceWordInFile(string path, string oldWord, string newWord)
+        {
+            string message = "null";
+
+            using (FileStream stream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                message = Encoding.UTF8.GetString(buffer); //input
+
+                //message = message.Replace(oldWord, newWord);
+
+                string pattern = oldWord; // это шаблон
+                string replacement = newWord; // это то, на что мы заменяем
+
+                message = Regex.Replace(message, pattern, replacement, RegexOptions.IgnoreCase);
+            }
+
+            using (FileStream stream = new FileStream(path, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[message.Length];
+                buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
     }
 }
